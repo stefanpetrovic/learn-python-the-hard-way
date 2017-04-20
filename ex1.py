1d772da0834d57a9f633349a2004185968e04d2d
stefanpetrovic <petrovicstefan91@gmail.com>
2017-04-20 09:37:41 +0200

    added tips and tricks file

--- ex1.py
+++ ex1.py
@@ -1,2 +1,11 @@
 print "Hello world"
 #print "hello"
+
+print 7.0 / 4.0
+print 7.0 // 4.0
+print 7 / 4
+print 7 // 4
+
+print -7 / 4
+
+print -7 // 4
---
+++ examples.py
@@ -0,0 +1,14 @@
+string = "adf", "af"
+
+print string
+
+print "asdf", "asdf"
+
+j = 1000
+
+while True:
+    for i in ["/", "-", "|", "\\", "|"]:
+        print "%s\r" % i
+
+    j = j - 1
+
